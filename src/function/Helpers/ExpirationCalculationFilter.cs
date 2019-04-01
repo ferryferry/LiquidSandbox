ce7c672a67d23dd01b680aa2bee7f960240adbba
Ferry Jongmans <ferryjongmans@Ferrys-MacBook-Pro.local>
2019-04-01 10:29:50 +0200

    Added comments and renamed the function

--- src/function/Helpers/ExpirationCalculationFilter.cs
+++ src/function/Helpers/ExpirationCalculationFilter.cs
@@ -3,26 +3,31 @@ using System.Text.RegularExpressions;
 
 namespace MasterData.Repositories.Helpers
 {
-    public static class  ExpirationCalculationFilter
+    public static class ExpirationCalculationFilter
     {
-        public static string Textilize(string expirationCalculation)
+        /// <summary>
+        /// This filter extracts the BusinessCentral expiration calculation to amount of days.
+        /// </summary>
+        /// <param name="expirationCalculation">A string which represents a timespan. I.e: 1Y6M2W</param>
+        /// <returns>Amount of days in string format. (convert to decimal if used in calculations</returns>
+        public static string ExpirationCalculation(string expirationCalculation)
         {
-            if(string.IsNullOrWhiteSpace(expirationCalculation) || string.IsNullOrEmpty(expirationCalculation))
-                return null;
-            
+            if (string.IsNullOrWhiteSpace(expirationCalculation) || string.IsNullOrEmpty(expirationCalculation))
+                return "null";
+
             var regex = new Regex("(\\d+)([dmwy|DMWY])");
 
             var matches = regex.Matches(expirationCalculation);
             if (matches == null || matches.Count == 0)
-                return null;
-            
+                return "null";
+
             var period = new TimeSpan();
-            
+
             foreach (Match match in matches)
             {
                 var amount = Convert.ToInt32(match.Groups[1].Value);
                 var unit = match.Groups[2].Value.ToUpper()[0];
-                
+
                 switch (unit)
                 {
                     case 'D':
@@ -39,7 +44,7 @@ namespace MasterData.Repositories.Helpers
                         break;
                 }
             }
-            
+
             return period.TotalDays.ToString();
         }
     }
